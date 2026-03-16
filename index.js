7815d471fc553706977bda7b0847b8c36c285954
Adnan4A <hmzali24@gmail.com>
2026-03-15 23:15:36 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const https = require('https');
+const http = require('http');
 
 const app = express();
+
+app.get('/test', function(req, res) {
+  http.get('http://realsport.scalecdn.co:8080', function(r) {
+    res.send('Connected! Status: ' + r.statusCode);
+  }).on('error', function(e) {
+    res.send('Failed to connect: ' + e.message);
+  });
+});
 
 app.use('/stream', createProxyMiddleware({
   target: 'http://realsport.scalecdn.co:8080',
   changeOrigin: true,
   pathRewrite: { '^/stream': '' },
-  on: {
-    proxyRes: function(proxyRes) {
-      proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-    }
+  headers: {
+    'Access-Control-Allow-Origin': '*'
   }
 }));
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log('Proxy running');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', function() {
+  console.log('Proxy running on port ' + PORT);
 });
+```
+
+Then open this in your browser:
+```
+https://m3u8-production-aa08.up.railway.app/test
